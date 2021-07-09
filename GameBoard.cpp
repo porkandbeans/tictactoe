57868a135ca962e21c5a5b812703b2ef04d0c388
Charlie <voter96@gmail.com>
2021-07-09 13:21:15 +1200

    hotfixing

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -167,14 +167,20 @@ bool GameBoard::checkVictory()
     }
 
     // check diagonals
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+    if (board[0][0] != ' ')
     {
-        return true;
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        {
+            return true;
+        }
     }
 
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    if (board[0][2] != ' ')
     {
-        return true;
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        {
+            return true;
+        }
     }
 
     // no rows or columns found yet
@@ -196,5 +202,6 @@ bool GameBoard::checkVictory()
     that Ben implements in his tutorial, though I respect that he
     makes them for begginning learners and I'm primarily learning
     C++ syntax and low level concepts
+
     */
 }
